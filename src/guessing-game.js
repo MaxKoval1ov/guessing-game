02dde59194090ed82d85015ba786246e029e855f
Maksim <maxkov112211@gmail.com>
2021-09-05 22:37:14 +0300

    add:solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,28 @@
 class GuessingGame {
-    constructor() {}
+
+  min = 0;
+  max = 0
+    // constructor(min = 0, max = 0) {
+    //   this.min = min;
+    //   this.max = max;
+    // }
 
     setRange(min, max) {
-
+      this.min = min;
+      this.max = max;
     }
 
     guess() {
-
+      this.result = Math.ceil((this.min + this.max)/2)
+      return this.result;
     }
 
     lower() {
-
+      this.max = this.result;
     }
 
     greater() {
-
+      this.min = this.result;
     }
 }
 
